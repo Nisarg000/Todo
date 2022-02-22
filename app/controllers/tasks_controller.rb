d467a3c837406fe96fc7a408d42203d0e5a269fa
Nisarg000 <nisargjhatakia94@gmail.com>
2022-02-22 18:19:40 +0530

    this shall be my last commit to this branch as this did not work out like I expected it to be

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,5 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all #when the user goes to the homepage it will retrieve all the tasks from the database and display them
+    end
 end
